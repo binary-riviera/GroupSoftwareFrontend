9bd7616c65d0b9da7f498e3a962025ee3170195d
Connor Forsyth <connorsforsyth@gmail.com>
2020-02-24 23:03:54 +0000

    back buttons to sign out

--- logOut.js
+++ logOut.js
@@ -10,30 +10,30 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const out = document.getElementById("gamekeeperLogin");
+const out = document.getElementById("exitGame");
 
 out.addEventListener('click', e => {
+    console.log('logging out')
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        console.log()
+    }).catch(function(error) {
+        // An error happened.
+        var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
 
-firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-    console.log()
-  }).catch(function(error) {
-    // An error happened.
-    var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
+    });
 
-  });
-
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser){
-        console.log(firebaseUser);
-    }
-    else{
-        document.location.href = "introPage.html";
-    }
-});
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser){
+            console.log(firebaseUser);
+        }
+        else{
+            document.location.href = "introPage.html";
+        }
+    });
 
 });
 
