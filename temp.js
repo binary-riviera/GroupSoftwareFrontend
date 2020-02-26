2bf2baaab1c53995ebd171846ea761a2f40d332e
Daniel Cripps <binary.riviera@gmail.com>
2020-02-26 13:42:37 +0000

    Added location checker

--- temp.js
+++ temp.js
@@ -1,3 +1,11 @@
+// 4th decimal place
+var isAtLoc = function(curLang, curLong, locLang, locLong) {
+  if (curLang.toFixed(4) === locLang.toFixed(4) && curLong.toFixed(4) === locLong.toFixed()) {
+    return true;
+  }
+  return false;
+}
+
 var watchId = navigator.geolocation.watchPosition(function(position)
   {
     document.getElementById('lang').innerHTML = position.coords.latitude;
