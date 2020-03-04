367fd7a8c22dd1898366e18323df668e6b137291
Mgulu <mbongenigulu2009@yahoo.co.uk>
2020-03-04 23:31:38 +0000

    done

--- makePlayer.js
+++ makePlayer.js
@@ -1,11 +1,22 @@
 var database = firebase.database();
+
+
 name = localStorage.getItem("studentName");
 console.log("IN HERE WITH + "+name);
 
+
+var starCountRef = firebase.database().ref('gameCondition');
+
+starCountRef.on('value', function (snapshot) {
+  gameCond = snapshot.val();
+});
+
+
+coords = {lat:0,lng:0}
 firebase.database().ref('players/'+name).set({
     clues : 0,
     playerName: name,
-    playerCoordinates: 0,
+    playerCoordinates: coords,
     playerLocation:"FORUM",
     playerRoute:"Forum-Ram-Harrison",
   });
