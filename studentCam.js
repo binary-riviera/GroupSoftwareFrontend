460035a5037977ad8441b6ee702ad4af011a7206
Connor Forsyth <connorsforsyth@gmail.com>
2020-03-05 14:59:05 +0000

    pick random route

--- studentCam.js
+++ studentCam.js
@@ -1,19 +1,40 @@
 var current = 0;
 document.getElementById('waypointNumber').innerText = current;
 
-var locClues = db.collection('Locations').doc("49FQBOBFgnZsoHzGXjvc").get();
+// pick random route out of all Routes
+//find out how many Routes
+var count = 0;
+var routeIDs = [];
 
-locClues.then(function(doc) {
-  var setClue = doc.data().Clue;
-  document.getElementById('clue').innerText = setClue;
+db.collection('Routes').get().then((snapshot) => {
+  snapshot.docs.forEach(doc => {
+    count += 1;
+    routeIDs.push(doc.id);
+  });
+  var ran = Math.floor(Math.random() * count);
+  currentRoute = routeIDs[ran];
+  var col = db.collection('Routes').doc(currentRoute).get();
+
+  //get id of first location
+  col.then(function(doc) {
+    var loc = doc.data().Locations;
+    var id = loc[0].id.replace(/\s/g, '');
+    //set first clue
+    var locClues = db.collection('Locations').doc(id).get();
+
+    locClues.then(function(doc) {
+      var setClue = doc.data().Clue;
+      document.getElementById('clue').innerText = setClue;
+    });
+  });
 });
-
-
 
 
 function updateClue(result) {
 
-  // pick random route out of all Routes
+
+  var ran = Math.floor(Math.random() * 11);
+
   var col = db.collection('Routes').doc("q2B78ABFQ0pDPom2Sxor").get();
 
   //get current id
@@ -53,8 +74,7 @@ function openCamera() {
   let scanner = null;
   Dynamsoft.BarcodeScanner.createInstance({
     // GETS THE RESULT
-    onFrameRead: results => {
-    },
+    onFrameRead: results => {},
     onUnduplicatedRead: (txt, result) => {
       updateClue(txt);
     }
