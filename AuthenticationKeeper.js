ded05975d848107eaea1b305bdc9da8a2a10ee1e
Connor Forsyth <connorsforsyth@gmail.com>
2020-03-05 14:20:49 +0000

    key value connected with firestore to validate game keeper authentication

--- AuthenticationKeeper.js
+++ AuthenticationKeeper.js
@@ -6,23 +6,42 @@
  * @author Connor Forsyth
  * @since  20/2/2020
  */
+const db = firebase.firestore();
 
 const login = document.getElementById('login');
+
+
 login.addEventListener('click', e => {
 
   const emailTxt = document.getElementById('email_address');
   const passwordTxt = document.getElementById('password');
+  const keyTxt = document.getElementById('key');
 
   var email = emailTxt.value;
   var pass = passwordTxt.value;
+  var keyVal = keyTxt.value;
+  console.log(keyVal);
 
-  firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+  db.collection('Key').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+      var code = doc.data().code;
 
-    alert("Password or User Name are incorrect. Please Try Again");
-    var errorCode = error.code;
-    var errorMessage = error.message;
+      if(code == keyVal){
+        console.log(code);
+        firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+
+          alert("Password or User Name are incorrect. Please Try Again");
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+      }else{
+        alert("Key is incorrect. Please Try Again");
+      }
+    });
   });
+
 });
+
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if (firebaseUser) {
